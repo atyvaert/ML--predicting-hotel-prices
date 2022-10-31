a1e9475891f903d2e0f05b8f6aa3b40582b25360
atyvaert <Artur.Tyvaert@UGent.be>
2022-10-31 12:15:24 +0100

    streamlined

--- src/Data_exploration.R
+++ src/Data_exploration.R
@@ -1,11 +1,16 @@
 ##############################################################
 ##############################################################
-# Data Import
+# 1. Data Import
 ##############################################################
 ##############################################################
+rm(list = ls())
 setwd(dir = '/Users/Artur/Desktop/uni jaar 6 sem 1/machine learning/groepswerk/R/data')
 train <- read.csv('./train.csv')
 test_X <- read.csv('./test.csv')
+
+
+# packages and libraries needed
+library(stringr)
 
 # Look at the data
 summary(train)
@@ -16,67 +21,107 @@ train$average_daily_rate
 train_X <- subset(train, select = -c(average_daily_rate))
 train_y <- train$average_daily_rate
 
-# look at the structure of train datasets
+# inspect
 str(train_X)
-str(train_y) # moet nog omgezet worden naar numeric vector
+str(train_y)
+# We have a problem with train_y, as it is a character type and it is accompanied
+# with some random nr each time that does not belong to this variable
+# Therefore, we keep the first part of the string character as a numeric variable
+train_y <- as.numeric(word(train_y, 1))
+
+# Again inspect the structure of train_y
+str(train_y) 
 
 
 
 ##############################################################
 ##############################################################
-# Data exploration
+# 2. Data exploration
 ##############################################################
 ##############################################################
 
-# 1) Arrival dates
-# have not looked at dates yet on dodona
-# get month and day of the week out of it
+# 1 Arrival dates
+train_X$arrival_date
+# IMPORTANT: WEEKDAY NEEDS TO BE ADDED
+# parse date with posixct: 
 
-# 2) assigned_room_type
+# 2 assigned_room_type
 barplot(table(train_X$assigned_room_type))
 table(train_X$assigned_room_type)
-# 2 categorie??n met bijna geen enkele observatie
+# 2 categorieën met bijna geen enkele observatie
 # Wat doen we hiermee?
+# Op zich kunnen we erin laten want geen misssing values
+# nominal feature: dummy encoding
 
-# 3) booking agent
+# 3 booking_agent
 barplot(table(train_X$booking_agent))
 table(train_X$booking_agent)
-# lots of different values, should be made numeric?
+# lots of different values or NULL values (11 361): is niet gelijk aan Na values
+# Maak variabele aan die aangeeft als booking agent wel (=1) of niet (= 0) aanwezig was:
+# booking_agent_present
+# => binary feature: integer encoding (indicator variable)
 
-# 4) booking company
+
+# 4 booking_company
 barplot(table(train_X$booking_company))
 table(train_X$booking_company)
-# Bijna allemaal NULL values
+# lots of different values or NULL values (78 303): is niet gelijk aan Na values
+# Maak variabele aan die aangeeft als booking company wel (=1) of niet (= 0) aanwezig was:
+# booking_company_present
+# => binary feature: integer encoding (indicator variable)
 
-# 5) booking_distribution_channel
+
+# 5 booking_distribution_channel
 barplot(table(train_X$booking_distribution_channel))
 table(train_X$booking_distribution_channel)
-# only 2 undefined dus zou met modus imputen of toch laten?
+# only 2 undefined dus zou ditlaten, op zich geen Na?
+# other option: impute with modus and treat undefined as Na
+# nominal feature: dummy encoding
 
-# 6) canceled
+# 6 canceled
 barplot(table(train_X$canceled))
 table(train_X$canceled)
 # geen problemen met variabelen, enkel nog dummies ofzo van maken
+# # nominal feature: dummy encoding OF binary feature: integer encoding (denk dit)
 
-# 7) car_parking_spaces
+# 7 car_parking_spaces
 hist(train_X$car_parking_spaces)
-boxplot(train_X$car_parking_spaces)
-# enkel 0 of 1 (informatie voor encoding)
+quantile(train_X$car_parking_spaces, na.rm = T, probs = seq(0, 1, 0.01))
+summary(train_X$car_parking_spaces)
+# van 0 tot 3 (informatie voor encoding)
+# 93% die nul hebben als value, 6% heeft 1 en 1% die 2 of 3 hebben
+# zal zich waarschijnlijk oplossen bij behandelen van outliers
+# dus binary: integer encoding
 
-#8 country
+# 8 country
+barplot(table(train_X$country))
 table(train_X$country)
+unique(train_X$country)
+# heel wat verschillende landen (164)
+# nominal feature: dummy encoding maar beperkt tot x aantal belangrijkste
+# zou persoonlijk voor 11 gaan obv die barplot
+cut_off_point <- as.numeric(table(train_X$country))
+cut_off_point_perc <- cut_off_point/(sum(cut_off_point))
+sort(cut_off_point_perc, decreasing  = T)
+# 13 dummies hierop gebaseerd (tot het ten minste 0.01% van de data voorsteld)
+
+
 
 #15 last_status_date
-sum(is.na(train$last_status_date))
+train_X$last_status_date
+sum(is.na(train_X$last_status_date))
 # what to do with 2615 missing values (where are we going to use this column)
+# Na: look at average time since last_status_update for the others
+# use variable for hint but not in prediction model I think
 
 #16 lead_time
+train_X$lead_time
 boxplot(train_X$lead_time)
 hist(train_X$lead_time, breaks = 1000)
 #0 when cancelled
 
 #17 market_segment
-table(train_X$market_segment)
+table(train_X$market_segment) # no problems
 
 #18 meal_booked
 table(train_X$meal_booked)
@@ -85,10 +130,12 @@ table(train_X$meal_booked)
 #19 nr_adults
 table(train_X$nr_adults)
 #values 26, 27 -> large groups (cancelled)
+# Vraag: wat hiermee doen?
 
 #20 nr_babies
 table(train_X$nr_babies)
 #string n/ = 0
+# 9 als valid of invalid outlier?
 
 #21 nr_booking_changes  
 barplot(table(train_X$nr_booking_changes))
--- src/data_cleaning.R
+++ src/data_cleaning.R
@@ -1,68 +1,192 @@
+##############################################################
+##############################################################
+# Data Cleaning
+##############################################################
+##############################################################
+
+# packages and libraries needed
 library(readr)
 library(dummy)
-
 library(stringr)
 
-#READ
+# import the data
+setwd(dir = '/Users/Artur/Desktop/uni jaar 6 sem 1/machine learning/ml22-team10/data/bronze_data')
+train <- read.csv('./train.csv')
+test_X <- read.csv('./test.csv')
+
+# for Viktor:
 # train <- read_csv("C:/Users/vikto/OneDrive - UGent/TweedeMaster/MachineLearning/ML_Team10/data/raw_data/train.csv")
-train <- read.csv("data/raw_data/train.csv")
+# train <- read.csv("data/raw_data/train.csv")
+
+# separate dependent and independent variables
 train_X <- subset(train, select = -c(average_daily_rate))
 train_y <- train$average_daily_rate
+train_y <- as.numeric(word(train_y, 1))
 
-#Create getmode function
-# Create the function.
-getmode <- function(v) {
-  uniqv <- unique(v)
-  uniqv[which.max(tabulate(match(v, uniqv)))]
+
+##############################################################
+##############################################################
+# 1. Adjust structure + Parse dates
+##############################################################
+##############################################################
+# Parse arrival_date to filter out the month, day of the month and year
+train_X$arrival_date
+train_X$posix <- as.POSIXct(train_X$arrival_date, format="%B  %d  %Y")
+train_X$day_of_month <- format(train_X$posix, format = '%d')
+train_X$month <- format(train_X$posix, format = '%B')
+train_X$year <- format(train_X$posix, format = '%Y')
+
+#16 lead_time to integer number of days
+train_X$lead_time <- substr(train_X$lead_time, start = 1, stop = (nchar(train_X$lead_time)-7))
+train_X$lead_time <- as.numeric(train_X$lead_time)
+train_X$lead_time
+
+
+##############################################################
+##############################################################
+# 2. Missing values
+##############################################################
+##############################################################
+
+# overwrite existing dataframes
+train_X_impute <- train_X
+test_X_impute <- test_X
+
+##############################################################
+# 2.1 Detecting 
+##############################################################
+# training data
+colMeans(is.na(train_X_impute))
+# test data
+colMeans(is.na(test_X_impute))
+
+##############################################################
+# 2.2 Missing value imputation + Flagging
+##############################################################
+# Missing values might increase model performance if they are informative, therefore
+# we add a NA indicator of the missing values. We do this by making use of the function naFlag:
+
+naFlag <- function(df, df_val = NULL) {
+  if (is.null(df_val)) {
+    df_val <- df
+  }
+  mask <- sapply(df_val, anyNA)
+  out <- lapply(df[mask], function(x)as.numeric(is.na(x)))
+  if (length(out) > 0) names(out) <- paste0(names(out), "_flag")
+  return(as.data.frame(out))
 }
 
-#1 arrival_date to Date
-train_X$arrival_date <- parse_date(train_X$arrival_date, format = "%B  %d  %Y")
+# Flag the missing values
+train_X_impute <- cbind(train_X_impute, naFlag(df = train_X_impute))
+test_X_impute <- cbind(test_X_impute,naFlag(df = test_X_impute, df_val = train_X_impute))
 
-#16 lead_time to integer number of days and impute NA
-train_X$lead_time <- substr(train_X$lead_time, start = 1, stop = (nchar(train_X$lead_time)-7))
-train_X_impute <- train_X
-train_X_impute$lead_time <- as.numeric(train_X$lead_time)
-train_X_impute$lead_time[is.na(train_X_impute$lead_time)] <- round(mean(as.integer(train_X$lead_time), na.rm = TRUE))
-train_X <- train_X_impute
+# inspect
+str(train_X_impute)
+# str(test_X_impute)
 
+# 2.2.1 Impute NUMERIC variables with information from the training set
 
-
-
-#CUSTOMER TYPE, DAYS IN WAITING LIST, DEPOSIT, HOTEL TYPE, IS_REPEATED_GUEST, LAST_STATUS
-#IMPUTING
-# create function to impute missing values 
-impute <- function(x, method = mean, val = NULL) {
-  if (is.null(val)) {
-    val <- method(x, na.rm = TRUE)
+# use the 'impute' function for numeric predictors:
+impute <- function(x, method = mean, val = NULL){
+  if(is.null(val)){
+    val <- method(x, na.rm = T)
   }
   x[is.na(x)] <- val
   return(x)
 }
-# create a function to calculate the mode
+
+num.cols <- c('car_parking_spaces', 'days_in_waiting_list')
+train_X_impute[, num.cols] <- lapply(train_X_impute[, num.cols], 
+                                     FUN = impute,
+                                     method = mean)
+
+quantile(train_X_impute$car_parking_spaces, na.rm = T, probs = seq(0, 1, 0.01))
+
+test_X_impute[, num.cols] <- mapply(test_X_impute[, num.cols],
+                                    FUN = impute,
+                                    val = colMeans(train_X[, num.cols], na.rm = T))
+
+
+# 2.2.1 Impute CATEGORICAL variables with information from the training set
+# use the 'modus' function for categorical predictors. This returns the mode of a column.
 modus <- function(x, na.rm = FALSE) {
   if (na.rm) x <- x[!is.na(x)]
   ux <- unique(x)
   return(ux[which.max(tabulate(match(x, ux)))])
 }
-# impute missing values with the mode of the customer type column 
-train_X_impute$customer_type <- impute(train_X_impute$customer_type, method = modus)
-train_X_impute$customer_type
-# impute missing values with the mean of the days in waiting lest column 
-train_X_impute$days_in_waiting_list <- impute(train_X_impute$days_in_waiting_list, method = mean)
-train_X_impute$days_in_waiting_list
-# impute missing values with the mode of the deposit column
-train_X_impute$deposit <- impute(train_X_impute$deposit, method = modus)
-train_X_impute$deposit
-# impute missing values with the mode of the hotel type column 
-train_X_impute$hotel_type <- impute(train_X_impute$hotel_type, method = modus)
-train_X_impute$hotel_type
-# impute missing values with the mode of the is repeated guest column 
-train_X_impute$is_repeated_guest <- impute(train_X_impute$is_repeated_guest, method = modus)
-train_X_impute$is_repeated_guest
-# impute missing values with the mode of the last_status column 
-train_X_impute$last_status <- impute(train_X_impute$last_status, method = modus)
-train_X_impute$last_status
+
+# handle
+cat.cols <- c('booking_distribution_channel', 'country', 'customer_type', 'deposit', 'hotel_type',
+              'is_repeated_guest', 'last_status', 'market_segment', 'nr_adults', 'nr_children',
+              'nr_previous_bookings', 'previous_bookings_not_canceled', 'previous_cancellations')
+train_X_impute[, cat.cols] <- lapply(train_X_impute[, cat.cols],
+                                     FUN = impute,
+                                     method = modus)
+test_X_impute[, cat.cols] <- mapply(test_X_impute[, cat.cols],
+                                    FUN = impute,
+                                    val = sapply(train_X[, cat.cols], modus, na.rm = T))
+
+
+# impute 'n/a' or Na values with 0 for nr_babies and nr_booking_changes as 'n/a' suggests
+# a value of zero
+# for 'n/a':
+train_X_impute$nr_babies <- as.numeric(str_replace_all(train_X$nr_babies, "n/a", "0"))
+test_X_impute$nr_babies <- as.numeric(str_replace_all(test_X_impute$nr_babies, "n/a", "0"))
+
+# for Na values
+train_X_impute$nr_booking_changes[is.na(train_X_impute$nr_booking_changes)] <- 0
+test_X_impute$nr_booking_changes[is.na(test_X_impute$nr_booking_changes)] <- 0
+
+
+# inspect
+colMeans(is.na(train_X_impute))
+colMeans(is.na(test_X_impute))
+
+
+
+##############################################################
+##############################################################
+# 3. Detecting and handling outliers
+##############################################################
+##############################################################
+train_X_outlier <- train_X_impute
+
+# make a vector of all the variables of which valid outliers need to be handled
+outlier.cols <- c()
+# look at all the numeric variables and detect valid and invalid outliers:
+# 1) car_parking_spaces
+car_parking_spaces_z <- scale(train_X_impute$car_parking_spaces)
+quantile(car_parking_spaces_z, na.rm = T, probs = seq(0, 1, 0.01))
+# hier zien we wel wat outliers, met een maximum van 3 parking spaces
+# dit lijken valid outliers (3 parking spaces required by customer is possible)
+# add car_parking_spaces to variables that need to be handled
+outlier.cols <- append(outlier.cols, 'car_parking_spaces')
+
+
+
+
+# use this function to handle valid outliers
+handle_outlier_z <- function(col){
+  col_z <- scale(col)
+  ifelse(abs(col_z)>3,
+         sign(col_z)*3*attr(col_z,"scaled:scale") + attr(col_z,"scaled:center"), col)
+}
+
+# handle all the outlier at once
+train_X_outlier[, outlier.cols] <-  sapply(train_X_impute[, outlier.cols], FUN = handle_outlier_z)
+
+
+
+#WRITE
+write.csv(train,"data/silver_data/train.csv", row.names = FALSE)
+write.csv(train_X,"data/silver_data/train_X.csv", row.names = FALSE)
+write.csv(train_y,"data/silver_data/train_y.csv", row.names = FALSE)
+
+
+##########@
+# HOORT BIJ FE EN NIET HIER
+###########
+
 
 #ENCODING 
 # get all columns that are categorical variables 
@@ -72,72 +196,3 @@ df_merge <- cbind(X = train_X_encode, y = train_y)
 # encode the variables
 train_X_encode <- model.matrix(y ~ . - 1, data = df_merge)
 train_X_encode
-
-
-# rm(list=ls()) 
-
-#17 impute market_segment with mode
-train_X_impute <- train_X
-train_X_impute$market_segment[is.na(train_X_impute$market_segment)] <- getmode(train_X$market_segment)
-train_X <- train_X_impute
-
-#19 impute nr_adults with mode
-train_X_impute <- train_X
-train_X_impute$nr_adults[is.na(train_X_impute$nr_adults)] <- getmode(train_X$nr_adults)
-train_X <- train_X_impute
-
-#20 impute nr_babies: "n/a" -> 0
-train_X_impute <- train_X
-train_X_impute$nr_babies <- as.numeric(str_replace_all(train_X$nr_babies, "n/a", "0"))
-train_X <- train_X_impute
-
-#---------
-#21 impute nr_booking_changes: "n/a" -> 0
-train_X_impute <- train_X
-train_X_impute$nr_booking_changes[is.na(train_X_impute$nr_booking_changes)] <- 0
-train_X <- train_X_impute
-
-#22 impute nr_children with mode (0)       
-train_X_impute <- train_X
-train_X_impute$nr_children[is.na(train_X_impute$nr_children)] <- getmode(train_X$nr_children)
-train_X <- train_X_impute
-
-#23 impute nr_nights
-# No Na values!
-
-#24 impute nr_previous_bookings with mode (0)  
-train_X_impute <- train_X
-train_X_impute$nr_previous_bookings[is.na(train_X_impute$nr_previous_bookings)] <- getmode(train_X$nr_previous_bookings)
-train_X <- train_X_impute
-
-#25 impute previous_bookings_not_canceled with mode (0) 
-train_X_impute <- train_X
-train_X_impute$previous_bookings_not_canceled[is.na(train_X_impute$previous_bookings_not_canceled)] <- getmode(train_X$previous_bookings_not_canceled)
-train_X <- train_X_impute
-
-#26 impute previous_cancellations with mode (0)   
-train_X_impute <- train_X
-train_X_impute$previous_cancellations[is.na(train_X_impute$previous_cancellations)] <- getmode(train_X$previous_cancellations)
-train_X <- train_X_impute
-
-#27 reserved_room_type  
-# No Na values!
-
-#28 special_requests  
-# No Na values!
-
-
-
-
-#--------
-
-
-
-
-#train_y
-train_y <- as.double(gsub('.{6}$', '', train_y))
-
-#WRITE
-write.csv(train,"data/silver_data/train.csv", row.names = FALSE)
-write.csv(train_X,"data/silver_data/train_X.csv", row.names = FALSE)
-write.csv(train_y,"data/silver_data/train_y.csv", row.names = FALSE)
